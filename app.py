40ec6051a939bfa815f02e1422d6fe957a4c295f
Emmanuel Dan-Awoh <33968860+Enmerka@users.noreply.github.com>
2025-05-02 21:37:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -168,7 +168,7 @@ if submit_button:
 # Sidebar for app instructions
 st.sidebar.title("Hello, My Good Friend")
 st.sidebar.markdown("""
-### Join this [Community](https://chat.whatsapp.com/KiHGrvcJX0i8kXP18aL2g2) + [Watch Video](https://youtu.be/3YbJxxqWspo)
+### [Watch Video](https://youtu.be/3YbJxxqWspo)
 
 This tool helps content teams calculate the compression ratio of pages in order assess the quality and relevance of their content. The compression ratio gives insights into the following:
 
